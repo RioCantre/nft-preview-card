1568fba342e1b6580014ec48bef59c372bca8551
Rio Cantre <smittenaf301icu@gmail.com>
2021-12-01 20:26:36 +0100

    Added Box2 style

--- src/Component/Box2/Box2.js
+++ src/Component/Box2/Box2.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Box2.css';
+import avatar from '../../images/image-avatar.png';
 
 function Box2() {
     return (
-        <div>
-        
-        </div>
+      <div className='box2-content'>
+        <img src={avatar} alt='avatar' />
+        <p className="box2-text">Creation of </p>
+        <p className="box2-name">Jules Wyvern</p>
+      </div>
     );
 }
 

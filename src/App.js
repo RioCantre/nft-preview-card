ea8bd3ec8a63454a6eb4c4b1e81404f0f60d926d
Rio Cantre <smittenaf301icu@gmail.com>
2021-12-01 21:01:22 +0100

    Added hover state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import images from './images/image-equilibrium.jpg';
+import iconView from './images/icon-view.svg';
 import './App.css';
 import Box1 from './Component/Box1/Box1';
 import Box2 from './Component/Box2/Box2';
@@ -8,8 +9,11 @@ function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <div>
-                    <img src={images} alt='img' />
+                <div className="hero-image">
+                    <img src={images}  className= "main-image" alt='img' />
+                    <div className="image-hover">
+                        <img src={iconView} className="icon-view" alt="icon view"/>
+                    </div>
                 </div>
                 <a href='https://fonts.google.com/'>Equilibrium #3429</a>
                 <p>Our Equilibrium collection promotes balance and calm.</p>

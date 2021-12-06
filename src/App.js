70bd7e8f89d69dfc7e6a6e16f445419f9f7d3bfe
Rio Cantre <smittenaf301icu@gmail.com>
2021-12-06 14:23:07 +0100

    Added Attribution

--- src/App.js
+++ src/App.js
@@ -4,29 +4,26 @@ import iconView from './images/icon-view.svg';
 import './App.css';
 import Box1 from './Component/Box1/Box1';
 import Box2 from './Component/Box2/Box2';
+import Attribution from './Component/Attribution/Attribution';
 
 function App() {
     return (
-        <div className='App'>
+        <div>
+            <div className='App'>
             <header className='App-header'>
-            <div className='hero-image'>
+                <div className='hero-image'>
                 <img src={images} className='main-image' alt='img' />
                 <div className='image-hover'>
-                <img src={iconView} className='icon-view' alt='icon view' />
+                    <img src={iconView} className='icon-view' alt='icon view' />
                 </div>
-            </div>
-            <a href='https://fonts.google.com/'>Equilibrium #3429</a>
-            <p>Our Equilibrium collection promotes balance and calm.</p>
-            <Box1 />
-            <Box2 />
+                </div>
+                <a href='https://fonts.google.com/'>Equilibrium #3429</a>
+                <p>Our Equilibrium collection promotes balance and calm.</p>
+                <Box1 />
+                <Box2 />
             </header>
-            <div class='attribution'>
-                Challenge by
-                <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
-                    Frontend Mentor
-                </a>
-                . Coded by <a href='#'>Rio Cantre</a>.
             </div>
+            <Attribution />
         </div>
     );
 }
---
+++ src/Component/Attribution/Attribution.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Attribution.css';
+
+
+function Attribution() {
+    return (
+      <div className='attribution'>
+        Challenge by
+        <a
+          className='link'
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Frontend Mentor.
+        </a>
+         Coded by
+        <a
+          href='https://www.frontendmentor.io/profile/RioCantre'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='link'
+        >
+          Rio Cantre.
+        </a>
+        
+      </div>
+    );
+}
+
+export default Attribution;
